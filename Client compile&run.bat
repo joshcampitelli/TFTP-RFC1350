636ea5f660e3ec3b998dfb426f51bcd0755e7218
ahmed sakr <ahmed.sakr@carleton.ca>
2017-05-07 16:59:55 -0400

    Commented FileTransfer class and added check for write()

--- Client compile&run.bat
+++ Client compile&run.bat
@@ -1,6 +1,6 @@
 @echo off
 title Client
 color 3f
-javac -d bin ./src/*.java ./src/core/*.java ./src/exceptions/*.java
+javac -d bin ./src/*.java ./src/core/*.java ./src/io/*.java ./src/exceptions/*.java
 java -classpath bin Client
 pause
