c23e7afec7e919db7c296f4b1f2a5a2c202fd5b7
Ahmed Sakr <ahmed.sakr@carleton.ca>
2017-05-08 16:10:56 -0400

    code changes, comments, and restructuring

--- src/Server.java
+++ src/Server.java
@@ -16,26 +16,13 @@ import exceptions.InvalidPacketException;
 public class Server extends SRSocket {
 
     private QuitListener quitListener;
-    private ArrayList<Thread> connections;
     private int threadNumber;
     public static int RECEIVE_PORT = 69;
 
     public Server() throws IOException {
         super("Server, Socket 'R'", RECEIVE_PORT);
-        this.connections = new ArrayList<>();
     }
 
-
-    /**
-     * Tests the packet's data buffer if it contains a valid client request.
-     *
-     * @return  true    if the packet contains a valid data buffer that may be processed
-     *          false   otherwise
-     */
-    public boolean validatePacket(DatagramPacket packet) {
-        byte[] readWriteValues = {1, 2};
-        return matches(packet.getData(), packet.getLength(), "0cx0x0", readWriteValues);
-    }
 
     /**
      * Attempts to establish a connection for the received packet. If the packet has been determined to be invalid,
@@ -43,18 +30,18 @@ public class Server extends SRSocket {
      *
      * @throws InvalidPacketException If the packet has been found to be illegal. Critical error.
      */
-    public void establish(DatagramPacket packet) throws IOException, InvalidPacketException {
-        if (!validatePacket(packet)) {
-            throw new InvalidPacketException("Invalid packet parsed. Aborting...");
-        }
-
+    public void establish(DatagramPacket packet) throws IOException {
         Connection connection = new Connection(packet);
         Thread thread = new Thread(connection, "Connection" + threadNumber++);
         thread.start();
-
-        connections.add(thread);
     }
 
+    /**
+     * Launches the operations of the server: starts listening for packets and establishes connections.
+     *
+     * @throws IOException
+     * @throws InvalidPacketException
+     */
     public void launch() throws IOException, InvalidPacketException {
         System.out.printf("Server has successfully launched.\n\n");
         QuitListener listener = new QuitListener(this, "QuitListener");
@@ -69,11 +56,13 @@ public class Server extends SRSocket {
         }
     }
 
+
+    /**
+     * Closes the receive socket of the server as a measure of not accepting anymore connections.
+     */
     public void shutdown() {
         close();
     }
-
-
 
     public static void main(String[] args) {
         Server server = null;
@@ -83,6 +72,7 @@ public class Server extends SRSocket {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InvalidPacketException e) {
+            server.shutdown();
             System.out.printf("Invalid packet encountered. Server shutting down: no more connections accepted.\n");
         }
     }
