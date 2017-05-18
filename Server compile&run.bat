5a923afa2acf025676c4e170a44edfda27b996e7
Ahmed Sakr <ahmed.sakr@carleton.ca>
2017-05-18 14:10:39 -0400

    Refactored structuring of the files and removed batch files

--- Server compile&run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Server
-color 3f
-javac -d bin ./src/com/tftp/*.java ./src/com/tftp/core/*.java ./src/com/tftp/io/*.java ./src/com/tftp/exceptions/*.java ./src/com/tftp/simulation/*.java ./src/com/tftp/workers/*.java
-java -classpath bin com.tftp.Server
-pause
