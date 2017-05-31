d55963e02723be50f2210de655e4a9afc9613f1c
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2017-05-30 22:00:31 -0400

    Fixed a few greasy bugs (#98)

--- src/com/tftp/Client.java
+++ src/com/tftp/Client.java
@@ -159,6 +159,7 @@ public class Client extends SRSocket {
                     //If response is a duplicate, that indicates that the connection never received the original
                     //ack and must resend the original ack.
                     response = receive(); //will be different for the wrq since its sending data....
+                    inform(response, "Received Packet");
                     continue;
                 }
 
@@ -234,23 +235,19 @@ public class Client extends SRSocket {
             if (Packet.getPacketType(response) == Packet.PacketTypes.ACK) {
                 ErrorStatus status = checkPacket(response, this.connectionTID, dataBlock - 1);
                 if (status == ErrorStatus.FATAL_ERROR) {
-                    System.out.println("FATAL ERROR DETECTED");
                     break;
                 } else if (status == ErrorStatus.NON_FATAL_ERROR) {
-                    System.out.println("NON-FATAL ERROR DETECTED");
+                    System.out.println();
                     if (fileTransfer.isComplete())
                         break;
 
                     response = receive();
                     continue;
                 } else if (status == ErrorStatus.DUPLICATE) {
-                    System.out.println("DUPLICATE PACKET DETECTED");
                     //If response is a duplicate, that indicates that the connection never received the original
                     //ack and must resend the original ack.
-                    inform(dataPacket, "Resending ACK Packet", true);
-                    send(dataPacket);
-                    response = this.waitForPacket(dataPacket);
-
+                    response = receive(); //will be different for the wrq since its sending data....
+                    inform(response, "Received Packet");
 
                     continue;
                 }
